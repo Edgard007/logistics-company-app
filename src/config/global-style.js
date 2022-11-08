ee6e42f0c0d68156f0f40a3fadef2f30f1b40cce
Edgardo Palacios <edgard07hp@gmail.com>
2022-11-08 15:32:08 -0600

    💄 Global style

--- src/config/global-style.js
+++ src/config/global-style.js
@@ -1,20 +1,22 @@
-import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 // ==> Global colours variable
 const themes = {
   primary: "#FFFFFF",
   secundary: "#F7F8F8",
-  blue: "#0052ff",
+  blue: "#3C5CA1",
   secBlue: "#3C5CA1",
   gray: "#DDDDDD",
   secGray: "#898989",
-  green: "#6EA03A",
+  black: "#232323",
 };
 
 // ==> Global styles
-const Wrapper = styled.div`
-  margin: 0px;
-  padding: 0px;
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0px;
+    padding: 0px;
+  }
 
   h1,
   h2,
@@ -24,6 +26,72 @@ const Wrapper = styled.div`
   h6 {
     margin-bottom: 0;
   }
+
+  /* Custom buttons */
+  .btn-Secundary {
+    background: ${(props) => props?.theme?.gray} !important;
+    color: ${(props) => props?.theme?.secGray} !important;
+    padding: 5px 25px !important;
+    border: 2px solid !important;
+    border-color: ${(props) => props?.theme?.secGray} !important;
+    border-radius: 15px;
+    font-weight: 400;
+    cursor: pointer;
+    outline: 0;
+  }
+
+  .btn-Secundary:hover {
+    background: ${(props) => props?.theme?.secGray} !important;
+    color: ${(props) => props?.theme?.primary} !important;
+    font-weight: 400;
+  }
+
+  /* Custom modals */
+  .ant-modal-content {
+    border-radius: 15px !important;
+    box-shadow: 0 0 20px 0${(props) => props?.theme?.black} !important;
+  }
+
+  .ant-modal-close {
+    border-radius: 15px !important;
+    background:${(props) => props?.theme?.primary} !important;
+    background-color:${(props) => props?.theme?.primary} !important;
+    color:${(props) => props?.theme?.secBlue} !important;
+    border: none !important;
+    padding: 0 !important;
+  }
+
+  .ant-modal-close:hover {
+    color:${(props) => props?.theme?.primary} !important;
+  }
+
+  .ant-modal-close {
+    position: absolute !important;
+    right: -30px !important;
+    top: -15px !important;
+    box-shadow: 0 0 20px 0${(props) => props?.theme?.black} !important;
+  }
+
+  .ant-modal-close:hover {
+    background:${(props) => props?.theme?.secGray} !important;
+  }
+
+  .ant-modal-close-x {
+    width: 28px !important;
+    height: 28px !important;
+  }
+
+  .ant-modal-close-x > .anticon {
+    vertical-align: 0.7em !important;
+  }
+
+  @media (max-width: 700px) {
+    .ant-modal-close {
+      position: absolute !important;
+      right: 10px !important;
+      top: 10px !important;
+    }
+  }  
 `;
 
-export { themes, Wrapper };
+export { themes, GlobalStyle };
--- src/index.js
+++ src/index.js
@@ -6,15 +6,14 @@ import "antd/dist/antd.min.css";
 import { ThemeProvider } from "styled-components";
 
 // ==> Config
-import { Wrapper, themes } from "./config/global-style";
+import { GlobalStyle, themes } from "./config/global-style";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <ThemeProvider theme={themes}>
-      <Wrapper>
-        <App />
-      </Wrapper>
+      <GlobalStyle />
+      <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
