6686be03622a38d5accc306f47f62d6b6534fbbf
Edgardo Palacios <edgard07hp@gmail.com>
2022-11-08 15:33:02 -0600

    📝 Use data API - add jsdoc

--- src/config/useDataApi.js
+++ src/config/useDataApi.js
@@ -3,6 +3,13 @@ const defaultHeaders = {
   "Content-Type": "application/json",
 };
 
+/**
+ * Hook to monitor the status of requests
+ * @param {string} path URL
+ * @param {'GET' | 'POST' | 'PUT' | 'DELETE'} [method] Method of request
+ * @param {object} body Body of request
+ * @param {object} headers Headers of request
+ */
 const requestApi = (path = "", method = "GET", body = {}, headers = {}) => {
   return new Promise((resolve, reject) => {
     fetch(path, {
@@ -11,15 +18,15 @@ const requestApi = (path = "", method = "GET", body = {}, headers = {}) => {
       body: Object.keys(body).length ? JSON.stringify({ ...body }) : null,
     })
       .then((response) => {
-        const status = response.status;
+        const status = response?.status;
+        const ok = response?.ok || false;
+
         response
           .json()
           .then((json) => {
-            resolve({ ok: status === 200, data: json, status: status });
+            resolve({ ok: ok, data: json, status: status });
           })
-          .catch(() =>
-            resolve({ ok: status === 200, data: [], status: status })
-          );
+          .catch(() => resolve({ ok: ok, data: [], status: status }));
       })
       .catch((e) => {
         console.error("||* ==> Error requestApi <== *||", e);
