533a49e9e10f3b57d6328a173268f648d2d80e99
Edgardo Palacios <edgard07hp@gmail.com>
2022-11-07 22:20:23 -0600

    💄 Setting global styles

---
+++ src/config/global-style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+// ==> Global colours variable
+const themes = {
+  primary: "#FFFFFF",
+  secundary: "#F7F8F8",
+  blue: "#0052ff",
+  secBlue: "#3C5CA1",
+  gray: "#DDDDDD",
+  secGray: "#898989",
+  green: "#6EA03A",
+};
+
+// ==> Global styles
+const Wrapper = styled.div`
+  margin: 0px;
+  padding: 0px;
+`;
+
+export { themes, Wrapper };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "antd/dist/antd.min.css";
+import { ThemeProvider } from "styled-components";
+
+// ==> Config
+import { Wrapper, themes } from "./config/global-style";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themes}>
+      <Wrapper>
+        <App />
+      </Wrapper>
+    </ThemeProvider>
   </React.StrictMode>
 );
